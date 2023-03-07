70b6c09f2df5005ad985420405087b477f55bc05
ACP0207\David.Pantoja <david.pantoja@alamoconsulting.com>
2023-03-07 18:04:28 +0100

    Creación proyecto jhipster

--- src/main/java/dev/gpi/g5/tapastopbackend/services/tapas/TapasService.java
+++
@@ -1,7 +0,0 @@
-package dev.gpi.g5.tapastopbackend.services.tapas;
-
-import dev.gpi.g5.tapastopbackend.models.Tapa;
-import dev.gpi.g5.tapastopbackend.services.BaseService;
-
-public interface TapasService extends BaseService<Tapa> {
-}
