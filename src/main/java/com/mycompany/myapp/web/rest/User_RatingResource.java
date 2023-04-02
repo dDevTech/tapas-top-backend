925fca0ab43c86d94e704daf4fb21353695987f8
jesusfl8 <58309329+jesusfl8@users.noreply.github.com>
2023-04-02 21:17:47 +0200

    rutas añadidas

--- src/main/java/com/mycompany/myapp/web/rest/User_RatingResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/User_RatingResource.java
@@ -1,10 +1,13 @@
 package com.mycompany.myapp.web.rest;
 
+import com.mycompany.myapp.domain.User_Rating;
 import com.mycompany.myapp.repository.User_RatingRepository;
 import com.mycompany.myapp.service.User_RatingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +24,25 @@ public class User_RatingResource {
         this.user_RatingRepository = user_RatingRepository;
         this.user_RatingService = user_RatingService;
     }
+
+    @RequestMapping(value = "/user-ratings", method = RequestMethod.GET)
+    public List<User_Rating> findAll() {
+        return user_RatingService.findAll();
+    }
+
+    @RequestMapping(value = "/user-ratings/{id}", method = RequestMethod.GET)
+    public User_Rating findById(@PathVariable("id") Long id) {
+        return user_RatingService.findById(id);
+    }
+
+    @RequestMapping(value = "/user-ratings", method = RequestMethod.POST)
+    public User_Rating save(@RequestBody User_Rating userRating) {
+        return user_RatingService.save(userRating);
+    }
+
+    @RequestMapping(value = "/user-ratings/{id}", method = RequestMethod.DELETE)
+    public void deleteById(@PathVariable("id") Long id) {
+        user_RatingService.deleteById(id);
+    }
+
 }
