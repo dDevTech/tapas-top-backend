74d697e4ecccb017c62c60784beab01069defa5b
edlv2001 <edlv2001@gmail.com>
2023-04-02 21:54:40 +0200

    Added CRUD Functionality for Tapa

--- src/main/java/com/mycompany/myapp/web/rest/TapaResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/TapaResource.java
@@ -1,11 +1,13 @@
 package com.mycompany.myapp.web.rest;
 
+import com.mycompany.myapp.domain.Tapa;
 import com.mycompany.myapp.repository.TapaRepository;
 import com.mycompany.myapp.service.TapaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tapa")
@@ -20,5 +22,26 @@ public class TapaResource {
     public TapaResource(TapaRepository tapaRepository, TapaService tapaService) {
         this.tapaRepository = tapaRepository;
         this.tapaService = tapaService;
+    }
+
+    @GetMapping("/all")
+    public List<Tapa> findAll(){
+        return this.tapaService.findAll();
+    }
+
+
+    @GetMapping("/byId")
+    public Tapa findById(@RequestParam Long id){
+        return this.tapaService.findById(id);
+    }
+
+    @PostMapping("/save")
+    public Tapa save(@RequestBody Tapa tapa){
+        return this.tapaService.save(tapa);
+    }
+
+    @DeleteMapping("/deleteById")
+    public void deleteById(@RequestParam Long id){
+        this.tapaService.deleteById(id);
     }
 }
