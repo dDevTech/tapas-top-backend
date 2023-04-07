eca13279fb8bfe5406a79ec0b4972d8f66da9c37
edlv2001 <edlv2001@gmail.com>
2023-04-07 21:47:12 +0200

    Tapa input modified to another dto

--- src/main/java/com/mycompany/myapp/web/rest/TapaResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/TapaResource.java
@@ -1,13 +1,17 @@
 package com.mycompany.myapp.web.rest;
 
+import com.mycompany.myapp.domain.Establishment;
 import com.mycompany.myapp.domain.Tapa;
+import com.mycompany.myapp.repository.EstablishmentRepository;
 import com.mycompany.myapp.repository.TapaRepository;
 import com.mycompany.myapp.service.TapaService;
+import com.mycompany.myapp.service.dto.TapaCreationDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tapa")
@@ -18,30 +22,39 @@ public class TapaResource {
     private final TapaService tapaService;
 
     private final TapaRepository tapaRepository;
+    private final EstablishmentRepository establishmentRepository;
 
-    public TapaResource(TapaRepository tapaRepository, TapaService tapaService) {
+    public TapaResource(TapaRepository tapaRepository, TapaService tapaService, EstablishmentRepository establishmentRepository) {
         this.tapaRepository = tapaRepository;
         this.tapaService = tapaService;
+        this.establishmentRepository = establishmentRepository;
     }
 
-    @GetMapping("/all")
+    @GetMapping("")
     public List<Tapa> findAll(){
         return this.tapaService.findAll();
     }
 
 
-    @GetMapping("/byId")
-    public Tapa findById(@RequestParam Long id){
+    @GetMapping("/{id}")
+    public Tapa findById(@PathVariable("id") Long id){
         return this.tapaService.findById(id);
     }
 
-    @PostMapping("/save")
-    public Tapa save(@RequestBody Tapa tapa){
-        return this.tapaService.save(tapa);
+    @PostMapping("")
+    public Tapa save(@RequestBody TapaCreationDTO tapa){
+        Establishment establishment;
+        System.out.println(tapa.toString());
+        if((establishment = this.establishmentRepository.findById(tapa.getEstablishment()).get()) == null){
+            return null;
+        }
+        Tapa t = tapa.toTapa();
+        t.setEstablishment(establishment);
+        return this.tapaService.save(t);
     }
 
-    @DeleteMapping("/deleteById")
-    public void deleteById(@RequestParam Long id){
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") Long id){
         this.tapaService.deleteById(id);
     }
 }
